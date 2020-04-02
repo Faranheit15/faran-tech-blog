8d3d2dc4f78e9122c82a7894a1cf7d2fc34ef528
Faran Mohammad <ffaranm15@gmail.com>
2020-04-02 12:15:32 +0530

    trial changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `FДЯДИHΞIΓ ΓΞCH ΓЯICҜS`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Faran Mohammad`,
+      summary: `, a developer and tech-enthusiast who lives and works in India building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `A hobby blog for Faran Mohammad.`,
+    siteUrl: `https://faranheit.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `faran.codes`,
     },
   },
   plugins: [
